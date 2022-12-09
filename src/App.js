57afb61f9543d52f2b5adc498af44a3b7f20fae0
Gustavo-M <gustavo-melo@outlook.com>
2022-12-09 17:05:02 -0300

    criando layout com Material-UI e consumindo api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
+import useStyles from "./App.styles"
+
+import { Button, Grid, TextField } from '@material-ui/core';
+
+const App = () => {
+
+    const classes = useStyles();
+
+    const [ form, setForm ] = useState({
+        city: "",
+    });
+    const handleSearchCity = () => {
+        axios.get(`http://api.weatherapi.com/v1/current.json?key=chave&q=${form.city}&lang=pt`)
+            .then(response => {
+                console.log(response.data);
+            })
+    }
+
+    const handleInputCity = (e) => {
+        const { name, value } = e.target;
+        setForm({
+            ...form,
+            [name]: value,
+        })
+    };
     return (
-        <div>
-            <h1>Weather</h1>
-        </div>
+        <>
+            <Grid container justifyContent="center">
+                <Grid item xs={12} md={3} className={classes.wrap}>
+                    <h1 className={classes.title}>Previsão do Tempo</h1>
+                    <p className={classes.subTitle}>Digite o nome da sua cidade para conferir como está o clima.</p>
+                    <div className={classes.divField}>
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            className={classes.field}
+                            name="city"
+                            value={form.city}
+                            onChange={handleInputCity}
+                        />
+                    </div>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => handleSearchCity()}
+                    >
+                        Pesquisar
+                    </Button>
+                </Grid>
+            </Grid>
+        </>
     )
 }
 
---
+++ src/App.styles.js
@@ -0,0 +1,27 @@
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+    wrap: {
+        backgroundColor: "#f4f4f4",
+        padding: 40,
+    },
+    title: {
+        paddingBottom: 20,
+        color: "#263238",
+    }, 
+    subTitle: {
+        paddingBottom: 20,
+        fontSize: 14,
+        color: "#263030",
+        fontWeight: "bold",
+    },
+    divField: {
+        marginBottom: 20,
+    },
+    field: {
+        backgroundColor: "#FFFFFF",
+    }
+}));
+
+export default useStyles;
